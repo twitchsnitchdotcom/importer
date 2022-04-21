3567a96de04625f06d635617b6a2ff3ee9582953
horizondeep <jamie@twitchsnitch.com>
2022-04-21 11:03:17 +0200

    bah

---
+++ src/test/java/com/twitchsnitch/importer/SpendingDotGov.java
@@ -0,0 +1,38 @@
+package com.twitchsnitch.importer;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.util.List;
+
+public class SpendingDotGov {
+
+    @Test
+    public void spendingDotGov() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        //File webDriverFile = new File("/usr/lib/chromium-browser/chromedriver");
+        File webDriverFile = new File("/Users/horizondeep/Desktop/importer/src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", webDriverFile.getAbsolutePath());
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+        options.addArguments("--disable-dev-shm-usage");
+        ChromeDriver driver = new ChromeDriver(options);
+
+        driver.get("https://sam.gov/data-services/Assistance%20Listings/usaspendinggov/historical?privacy=Public");
+        driver.get("https://sam.gov/data-services/Assistance%20Listings/usaspendinggov/historical?privacy=Public");
+
+        List<WebElement> elementsByClassName = driver.findElementsByClassName("data-service-file-link");
+        for(WebElement element: elementsByClassName){
+            element.click();
+            Thread.sleep(2000);
+        }
+    }
+}
