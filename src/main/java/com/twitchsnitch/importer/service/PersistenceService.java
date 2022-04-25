8ff1205739493fa2a38350605ae93d67c89cdd25
horizondeep <jamie@twitchsnitch.com>
2022-04-25 23:25:21 +0200

    commiting some files

--- src/main/java/com/twitchsnitch/importer/service/PersistenceService.java
+++ src/main/java/com/twitchsnitch/importer/service/PersistenceService.java
@@ -468,7 +468,7 @@ public class PersistenceService {
                         "                    l.is_mature = stream.is_mature WITH l, stream\n" +
                         "                    MERGE (u:User{login:stream.user_login, twitch_id:stream.user_id, name:stream.user_name})\n" +
                         "                    MERGE (u)-[:LIVE_STREAMING]->(l)\n" +
-                        "                    MERGE (l)-[:PLAYS]->(g:Game{twitch_id:stream.game_id})\n" +
+                        "                    MERGE (l)-[:PLAYS]->(g:Game{twitch_id:stream.game_id, name:stream.game_name})\n" +
                         "                    MERGE (lang:Language{key:stream.language})\n" +
                         "                    MERGE (l)-[:HAS_LANGUAGE]->(lang)\n" +
                         "                    SET u:Channel\n"
