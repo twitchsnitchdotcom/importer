2ed159bc511c08e94d7414c2ce10fb2d5612634b
jamie <jamie@twitchsnitch.com>
2022-05-10 15:51:15 +0000

    kinda working, nice :)

--- src/test/java/com/twitchsnitch/importer/JsoupTests.java
+++ src/test/java/com/twitchsnitch/importer/JsoupTests.java
@@ -1,8 +1,6 @@
 package com.twitchsnitch.importer;
 
-import com.twitchsnitch.importer.dto.sully.ChannelStatsSummaryDTO;
-import com.twitchsnitch.importer.dto.sully.IndividualChannelPageDTO;
-import com.twitchsnitch.importer.dto.sully.TeamsStatsSummaryDTO;
+import com.twitchsnitch.importer.dto.sully.*;
 import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,6 +16,183 @@ import java.util.List;
 
 public class JsoupTests {
 
+
+    @Test
+    public void homePageStats() throws IOException{
+        FileInputStream fis = new FileInputStream("src/main/resources/homepage.html");
+        String htmlString = IOUtils.toString(fis, "UTF-8");
+
+        HomePageDTO homePageDTO = new HomePageDTO();
+        Document doc = Jsoup.parse(htmlString);
+
+        //hourswatched
+        String hourswatched = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div.InfoStatPanelWrapper.InfoStatPanelSpacerLeft > div > div > div.InfoStatPanelTL > div").get(0).text();
+        String hourswatchedPercentage = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div.InfoStatPanelWrapper.InfoStatPanelSpacerLeft > div > div > div.InfoStatPanelTR > div > span").get(0).text();
+        String hourswatchedIndicatorClass = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div.InfoStatPanelWrapper.InfoStatPanelSpacerLeft > div > div > div.InfoStatPanelTR > div > span").get(0).className();
+        String hourswatchedChanged = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div.InfoStatPanelWrapper.InfoStatPanelSpacerLeft > div > div > div.InfoStatPanelBL > div").get(0).text();
+
+        homePageDTO.setHoursWatched();
+        homePageDTO.setHoursWatchedChangeAmount();
+        homePageDTO.setHoursWatchedChangeAmount();
+
+        //averageViewers
+        String averageViewers = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(2) > div > div > div.InfoStatPanelTL > div").get(0).text();
+        String averageViewersPercentage = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(2) > div > div > div.InfoStatPanelTR > div > span").get(0).text();
+        String averageViewersIndicatorClass = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(2) > div > div > div.InfoStatPanelTR > div > span").get(0).className();
+        String averageViewersChanged = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(2) > div > div > div.InfoStatPanelBL > div").get(0).text();
+
+        homePageDTO.setAverageViewers();
+        homePageDTO.setAverageViewersChangeAmount();
+        homePageDTO.setAverageViewersPercentageChange();
+
+        //peakViewers
+        String peakViewers = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(3) > div > div > div.InfoStatPanelTL > div").get(0).text();
+        String peakViewersPercentage = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(3) > div > div > div.InfoStatPanelTR > div > span").get(0).text();
+        String peakViewersIndicatorClass = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(3) > div > div > div.InfoStatPanelTR > div > span").get(0).className();
+        String peakViewersChanged = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(3) > div > div > div.InfoStatPanelBL > div").get(0).text();
+
+        homePageDTO.setPeakViewers();
+        homePageDTO.setPeakViewersChangeAmount();
+        homePageDTO.setPeakViewersPercentageChange();
+
+        //peakStreams
+        String peakStreams = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(4) > div > div > div.InfoStatPanelTL > div").get(0).text();
+        String peakStreamsPercentage = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(4) > div > div > div.InfoStatPanelTR > div > span").get(0).text();
+        String peakStreamsIndicatorClass = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(4) > div > div > div.InfoStatPanelTR > div > span").get(0).className();
+        String peakStreamsChanged = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(4) > div > div > div.InfoStatPanelBL > div").get(0).text();
+
+        homePageDTO.setPeakStreams();
+        homePageDTO.setPeakStreamsChangeAmount();
+        homePageDTO.setPeakStreamsPercentageChange();
+
+        //averageChannels
+        String averageChannels = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(5) > div > div > div.InfoStatPanelTL > div").get(0).text();
+        String averageChannelsPercentage = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(5) > div > div > div.InfoStatPanelTR > div > span").get(0).text();
+        String averageChannelsIndicatorClass = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(5) > div > div > div.InfoStatPanelTR > div > span").get(0).className();
+        String averageChannelsChanged = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(5) > div > div > div.InfoStatPanelBL > div").get(0).text();
+
+        homePageDTO.setAverageChannels();
+        homePageDTO.setAverageChannelsChangeAmount();
+        homePageDTO.setAverageChannelsPercentageChange();
+
+        //gamesStreamed
+        String gamesStreamed = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(6) > div > div > div.InfoStatPanelTL > div").get(0).text();
+        String gamesStreamedPercentage = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(6) > div > div > div.InfoStatPanelTR > div > span").get(0).text();
+        String gamesStreamedIndicatorClass = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(6) > div > div > div.InfoStatPanelTR > div > span").get(0).className();
+        String gamesStreamedChanged = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div:nth-child(6) > div > div > div.InfoStatPanelBL > div").get(0).text();
+
+        homePageDTO.setGamesStreamed(gamesStreamed);
+        homePageDTO.setGamesStreamedChangeAmount(gamesStreamedChanged);
+        homePageDTO.setGamesStreamedPercentageChange(gamesStreamedPercentage);
+
+        //viewerRatio
+        String viewerRatio = doc.select("body > div.RightContent > div.MainContent > div.InfoStatPanelContainerTop.InfoStatPanelContainerTopSpacer > div > div.InfoStatPanelWrapper.InfoStatPanelSpacerMiddle > div > div > div.InfoStatPanelTL > div").get(0).text();
+
+        homePageDTO.setViewerRatio(viewerRatio);
+        homePageDTO.setDayRange(7);
+
+        //mostWatchedChannelsRows
+        Elements mostWatchedChannelsRows = doc.select("body > div.RightContent > div.MainContent > div.PageContentContainer > div > div > div:nth-child(1) > div:nth-child(1)").get(0).getElementsByClass("InfoStatPanelLongInner");
+        List<HomePageElementDTO> mostWatchedChannels = getDTOsFromElements(mostWatchedChannelsRows);
+        //mostWatchedGamesRows
+        Elements mostWatchedGamesRows = doc.select("body > div.RightContent > div.MainContent > div.PageContentContainer > div > div > div:nth-child(1) > div.InfoStatPanelLong.InfoStatPanelLongMiddle").get(0).getElementsByClass("InfoStatPanelLongInner");
+        List<HomePageElementDTO> mostWatchedGames = getDTOsFromElements(mostWatchedGamesRows);
+        //trendingGamesRows
+        Elements trendingGamesRows = doc.select("body > div.RightContent > div.MainContent > div.PageContentContainer > div > div > div:nth-child(1) > div:nth-child(3)").get(0).getElementsByClass("InfoStatPanelLongInner");
+        List<HomePageElementDTO> trendingGames = getDTOsFromElements(trendingGamesRows);
+        //fastestGrowingChannelsRows
+        Elements fastestGrowingChannelsRows = doc.select("body > div.RightContent > div.MainContent > div.PageContentContainer > div > div > div:nth-child(2) > div:nth-child(1)").get(0).getElementsByClass("InfoStatPanelLongInner");
+        List<HomePageElementDTO> fastestGrowingChannels = getDTOsFromElements(fastestGrowingChannelsRows);
+        //mostStreamedGamesRow
+        Elements mostStreamedGamesRow = doc.select("body > div.RightContent > div.MainContent > div.PageContentContainer > div > div > div:nth-child(2) > div.InfoStatPanelLong.InfoStatPanelLongMiddle").get(0).getElementsByClass("InfoStatPanelLongInner");
+        List<HomePageElementDTO> mostStreamedGames = getDTOsFromElements(mostStreamedGamesRow);
+        //mostViewedStreamsRow
+        Elements mostViewedStreamsRow = doc.select("body > div.RightContent > div.MainContent > div.PageContentContainer > div > div > div:nth-child(2) > div:nth-child(3)").get(0).getElementsByClass("InfoStatPanelLongInner");
+        List<HomePageElementDTO> mostViewedStreams = getDTOsFromElements(mostViewedStreamsRow);
+
+
+
+        homePageDTO.setMostViewedStreams(mostViewedStreams);
+        homePageDTO.setFastestGrowingChannels(fastestGrowingChannels);
+        homePageDTO.setMostStreamedGames(mostStreamedGames);
+        homePageDTO.setMostViewedStreams(mostViewedStreams);
+        homePageDTO.setTrendingGames(trendingGames);
+        homePageDTO.setMostWatchedGames(mostWatchedGames);
+        homePageDTO.setMostWatchedChannels(mostWatchedChannels);
+
+        System.out.print(homePageDTO.toString());
+    }
+
+    private Double getPercentage(String value, String indicator){
+        if(){
+
+        }
+    }
+
+    private List<HomePageElementDTO> getDTOsFromElements(Elements elements){
+        List<HomePageElementDTO> homePageElementDTOS = new ArrayList<>();
+        int i = 0;
+        for(Element e: elements){
+            i++;
+            HomePageElementDTO homePageElementDTO = new HomePageElementDTO();
+
+            String channelUrl = e.getElementsByTag("a").get(0).attr("href");
+            String profileImage = e.getElementsByTag("a").get(0).getElementsByTag("img").get(0).attr("src");
+            String displayName = e.getElementsByTag("div").get(1).getElementsByTag("div").get(0).text();
+
+            String value = e.getElementsByClass("InfoStatPanelLongLabelMiddle").get(0).getElementsByClass("InfoStatPanelLongLabelMiddleTop").get(0).text();
+            String key = e.getElementsByClass("InfoStatPanelLongLabelMiddle").get(0).getElementsByClass("InfoStatPanelLongLabelMiddleBottom").get(0).text();
+
+            String percentageChange = null;
+            String positiveOrNegative = null;
+            String valueChange = null;
+
+            try{
+                percentageChange = e.getElementsByClass("InfoStatPanelLongRight").get(0).getElementsByClass("InfoStatPanelLongRightTop").get(0).getElementsByTag("span").get(0).text().replace("%", "");
+            }
+            catch(IndexOutOfBoundsException exception){
+                System.out.println("Percentage Change not available");
+            }
+
+            try{
+positiveOrNegative = e.getElementsByClass("InfoStatPanelLongRight").get(0).getElementsByClass("InfoStatPanelLongRightTop").get(0).getElementsByTag("span").get(0).className();
+
+            }
+            catch(IndexOutOfBoundsException exception){
+                System.out.println("positiveOrNegative Change not available");
+            }
+
+            try{
+                valueChange = e.getElementsByClass("InfoStatPanelLongRight").get(0).getElementsByClass("InfoStatPanelLongRightBottom").get(0).text();
+            }
+            catch(IndexOutOfBoundsException exception){
+                System.out.println("Value Change not available");
+            }
+
+
+            double percentageChanged = 0;
+
+            if(positiveOrNegative != null && percentageChange != null){
+                if(positiveOrNegative.equalsIgnoreCase("Positive")){
+                    percentageChanged = Double.parseDouble(percentageChange);
+                }
+                else{
+                    percentageChanged = Double.parseDouble("-" + percentageChange);
+                }
+            }
+
+            homePageElementDTO.setImage(profileImage);
+            homePageElementDTO.setKey(key);
+            homePageElementDTO.setName(displayName);
+            homePageElementDTO.setPercentageChange(percentageChanged);
+            homePageElementDTO.setValue(value);
+            homePageElementDTO.setRowNumber(i);
+            homePageElementDTO.setValueChange(valueChange);
+            System.out.println(homePageElementDTO.toString());
+            homePageElementDTOS.add(homePageElementDTO);
+        }
+        return homePageElementDTOS;
+    }
     @Test
     public void teamStatsPage() throws IOException {
         FileInputStream fis = new FileInputStream("src/main/resources/team-stats-page.html");
